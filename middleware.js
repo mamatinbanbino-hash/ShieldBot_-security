7abd722c436d1c13f7390a0bd8b25ca9f53c1d32
mamatinbanbino-hash <mamatinbanbino@gmail.com>
2026-03-03 05:56:10 +0000

    Delete middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-    const response = NextResponse.next();
-    // Signature Ndiaye Techn dans les Headers réels
-    response.headers.set('X-Audit-Security', 'Propulsed-by-NdiayeTechn');
-    response.headers.set('X-Content-Type-Options', 'nosniff');
-    return response;
-}
